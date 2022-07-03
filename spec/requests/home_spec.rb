b5b7122229b3588d72a239e5693c1b947fca7e7f
tmasuyama <tmasuyama1114@gmail.com>
2022-07-03 16:09:37 +0900

    Home#top のテストをリファクタ

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
-  describe "GET /top" do
-    it "returns http success" do
-      get "/home/top"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Home', type: :request do
+  describe 'GET /home/top' do
+    it 'HTTPステータス200を返す' do
+      get '/home/top'
+      expect(response).to have_http_status(200)
     end
   end
-
 end
