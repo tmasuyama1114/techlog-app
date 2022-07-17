c388658076bbfbfc2e19c9a6727c04cdec03d15e
tmasuyama <tmasuyama1114@gmail.com>
2022-07-17 11:04:24 +0900

    users テーブルのカラムが誤っていたので修正(name -> nickname)

--- db/migrate/20220716061809_add_nickname_to_user.rb
+++ db/migrate/20220716061809_add_nickname_to_user.rb
@@ -1,5 +1,5 @@
 class AddNicknameToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :name, :string
+    add_column :users, :nickname, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_16_061809) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
