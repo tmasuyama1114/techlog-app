05588d0d2ff5e6d324e81836cffdf48817b13998
tmasuyama <tmasuyama1114@gmail.com>
2022-07-17 11:05:24 +0900

    User.nickname の文字数に関するバリデーションを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,43 @@
 require 'rails_helper'
 
 describe User do
-  before do
-    create(:user, nickname: 'テスト太郎', email: 'test@example.com')
-  end
+  let(:nickname) { 'テスト太郎' }
+  let(:email) { 'test@example.com' }
 
   describe '.first' do
+    before do
+      create(:user, nickname: nickname, email: email)
+    end
+
     subject { described_class.first }
+
     it '事前に作成した通りのUserを返す' do
       expect(subject.nickname).to eq('テスト太郎')
       expect(subject.email).to eq('test@example.com')
+    end
+  end
+
+  describe 'validation' do
+    let(:password) { '12345678' }
+    describe 'nickname文字数制限の検証' do
+      context 'nicknameが20文字以下の場合' do
+        let(:nickname) { 'あいうえおかきくけこさしすせそたちつてと' } # 20文字
+
+        it 'User オブジェクトは有効である' do
+          user = User.new(nickname: nickname, email: email, password: password, password_confirmation: password)
+          expect(user.valid?).to be(true)
+        end
+      end
+
+      context 'nicknameが20文字を超える場合' do
+        let(:nickname) { 'あいうえおかきくけこさしすせそたちつてとな' } # 21文字
+
+        it 'User オブジェクトは無効である' do
+          user = User.new(nickname: nickname, email: email, password: password, password_confirmation: password)
+          user.valid?
+          expect(user.errors[:nickname]).to include('is too long (maximum is 20 characters)')
+        end
+      end
     end
   end
 end
