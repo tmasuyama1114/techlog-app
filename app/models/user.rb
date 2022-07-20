8dae9ff54b8a82e6c5a156aa624f8a9079e029c5
tmasuyama <tmasuyama1114@gmail.com>
2022-07-21 08:52:06 +0900

    :bulb: 不要なコメント削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, length: { maximum: 20 } # 変更
+  validates :nickname, presence: true, length: { maximum: 20 }
 end
