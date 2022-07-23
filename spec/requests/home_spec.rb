67343b863cf0954a7b27aeb5f908c4c264e305fc
tmasuyama <tmasuyama1114@gmail.com>
2022-07-23 14:20:18 +0900

    :recycle: トップページのパスを '/home/top' から '/' に変更

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Home', type: :request do
-  describe 'GET /home/top' do
+  describe 'GET /' do
     it 'HTTPステータス200を返す' do
-      get '/home/top'
+      get '/'
       expect(response).to have_http_status(200)
     end
   end
--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe 'Home', type: :system do
     driven_by :selenium_chrome_headless
   end
 
-  describe '/home/top 画面の検証' do
+  describe 'トップページの検証' do
     it 'Home#top という文字列が表示される' do
-      visit home_top_path # 名前付きパス
+      visit root_path
 
       expect(page).to have_content('Home#top')
     end
