c273523d98d7818a2c505f278a150b79f459a4b9
tmasuyama <tmasuyama1114@gmail.com>
2022-07-30 22:31:14 +0900

    :construction: PostsController(#new, #create)を作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id # ログインユーザのIDを代入
+    if @post.save
+      flash[:notice] = '投稿しました'
+      redirect_to root_path # 一時的にトップページへリダイレクト(要修正)
+    else
+      flash[:alert] = '投稿に失敗しました'
+      render new_post_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
